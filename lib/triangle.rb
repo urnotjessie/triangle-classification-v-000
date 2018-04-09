b713de36f6ee3dc4c1de01b4b0088bec9d84c18e
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-09 12:47:37 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,19 @@
 class Triangle
   # write code here
+  attr_accessor :l1, :l2, :l3
   def initialize(l1, l2, l3)
+    @l1 = l1
+    @l2 = l2
+    @l3 = l3
   end
+
+  def kind
+    if l1 == l2 && l2 == l3 && l1 > 0
+      :equilateral
+    elsif (l1 == l2 && l2 != l3) || (l1 == l3 && l3 != l2) || (l2 == l3 && l3 != l1)
+      :isosceles
+    elsif l1 != l2 && l2 != l3 && l1 != l3
+      :scalene
+    elsif l1 == l2
+    
 end
