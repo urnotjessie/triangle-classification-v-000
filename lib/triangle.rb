c9d4d4debb3a036aa649873fbd2a1680d509e2b4
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-09 12:56:13 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,12 +11,13 @@ class Triangle
     if @l1 > 0 && @l2 > 0 && @l3 > 0
       if @l1 == @l2 && @l2 == l3
         :equilateral
+      elsif @l1 + @l2 <= @l3 || @l1 + @l3 <= @l2 || @l2 + @l3 <= @l1
+        raise TriangleError
       elsif (@l1 == @l2 && @l2 != @l3) || (@l1 == @l3 && @l3 != @l2) || (@l2 == @l3 && @l3 != @l1)
         :isosceles
       elsif @l1 != @l2 && @l2 != @l3 && @l1 != @l3
         :scalene
-      elsif @l1 + @l2 <= @l3 || @l1 + @l3 <= @l2 || @l2 + @l3 <= @l1
-        raise TriangleError
+
       end
     else
       raise TriangleError
